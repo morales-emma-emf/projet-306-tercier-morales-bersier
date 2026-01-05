94bc9574e4187c03590b9fe3d97a5c87738b74df
Elouan <webacces.ch@gmail.com>
2026-01-05 15:52:09 +0100

    refactor: simplification de la redirection vers la page de connexion et mise à jour du middleware pour inclure le chemin /_next/data

--- code/web-app/app/page.tsx
+++ code/web-app/app/page.tsx
@@ -1,16 +1,5 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  const goToAuth = () => {
-    router.push("/login");
-  };
-
-  return (
-    goToAuth()
-  );
+  redirect("/login");
 }
