1d5e293b01f293f7fa6b4a126d8e20c31b9d8a94
Elouan <webacces.ch@gmail.com>
2026-01-05 15:47:47 +0100

    push

--- code/web-app/app/page.tsx
+++ code/web-app/app/page.tsx
@@ -1,27 +1,16 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function Home() {
+  const router = useRouter();
+
+  const goToAuth = () => {
+    router.push("/login");
+  };
+
   return (
-    <main className="home">
-      <section className="card">
-        <h1>Système de gestion des badges</h1>
-
-        <p className="subtitle">
-          Plateforme interne de gestion et d’attribution des badges
-        </p>
-
-        <p className="description">
-          Ce portail permet aux administrateurs de gérer les badges et aux
-          utilisateurs de consulter leur espace personnel.
-        </p>
-
-        <div className="buttons">
-          <Link href="/login" className="btn primary">
-            Se connecter
-          </Link>
-        </div>
-      </section>
-    </main>
+    goToAuth()
   );
 }
- 
